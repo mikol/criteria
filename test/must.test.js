c8622b9e405f6d05d969978366c90da512d8fe74
Mikol Graves <mikol@thinbox.org>
2016-03-02 08:52:02 -0800

    Add `must` assertions unit test.

---
+++ test/must.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('../lib/criteria/criteria'); /* globals scope, test */
+
+scope('Must Assertion Tests',
+function () {
+  scope.before(function () {
+    this.before = true;
+    this.error = new Error('');
+  });
+
+  test('Oughta pass.',
+  function (must) {
+    must.true(() => this.before === true);
+  });
+
+  test('Oughta throw.',
+  function (must) {
+    must.throw(() => this.before.call());
+  });
+
+  test('Oughta resolve.',
+  function (must) {
+    must.resolve(Promise.resolve(this.before), function (value) {
+      must.true(() => value === this.before);
+    });
+  });
+
+  test('Oughta reject.',
+  function (must) {
+    must.reject(Promise.reject(this.error), function (reason) {
+      must.true(() => reason === this.error);
+    });
+  });
+});
