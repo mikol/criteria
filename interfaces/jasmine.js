8ae968f7af4134af78ae0dab203ffda431a615f3
Mikol Graves <mikol@thinbox.org>
2016-03-02 09:02:38 -0800

    Fix interfaces `runner` import.

--- interfaces/jasmine.js
+++ interfaces/jasmine.js
@@ -8,7 +8,7 @@
 
 /* jshint ignore:start *//* jscs:disable */
 !function(e,o){'use strict';var t=[
-  '../runner'
+  '../lib/runner'
 ];if('function'==typeof define&&define.amd)define(t,function(){return o.apply(e,[].slice.call(arguments))});else if('object'==typeof module&&module.exports){for(var n=t.length;n--;)t[n]=require(t[n]);module.exports=o.apply(e,t)}}('object'==typeof global&&global||'object'==typeof window&&window||this,
 
 function (runner) {
